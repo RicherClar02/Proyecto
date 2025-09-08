c872847fb3350a8b91a05e38eaee6a5e5ea5d23e
go02 <brayanclarosdiaz@gmail.com>
2025-09-07 20:59:06 -0500

    cambios_mejora

--- next.config.mjs
+++ next.config.mjs
@@ -1,4 +1,28 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {};
+/** @type {import('next').NextConfig} */
+const nextConfig = {
+  output: 'standalone',
+  env: {
+    customKey: 'my-value',
+  },
+  images: {
+    domains: ['localhost'],
+  },
+  // Para evitar problemas de CORS
+  async headers() {
+    return [
+      {
+        source: '/api/:path*',
+        headers: [
+          { key: 'Access-Control-Allow-Origin', value: '*' },
+          { key: 'Access-Control-Allow-Methods', value: 'GET, POST, PUT, DELETE, OPTIONS' },
+          { key: 'Access-Control-Allow-Headers', value: 'Content-Type, Authorization' },
+        ],
+      },
+    ]
+  },
+}
+
+module.exports = nextConfig
 
 export default nextConfig;
